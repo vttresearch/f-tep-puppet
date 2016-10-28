cad69d3f92fbc20e755c7db54ed68933d8944615
Peter van Zetten <peter.van.zetten@cgi.com>
2016-10-28 10:47:49 +0100

    Initial commit: bare puppet module

---
+++ manifests/backend.pp
@@ -0,0 +1,10 @@
+# == Class: ftep::backend
+#
+# Install and manage the F-TEP processing back-end components, including
+# ZOO-Project WPS server and the F-TEP processors.
+#
+class ftep::backend(
+
+) {
+
+}
---
+++ manifests/portal.pp
@@ -0,0 +1,10 @@
+# == Class: ftep::portal
+#
+# Install and manage the F-TEP portal front-end components, including
+# Drupal and the F-TEP web application.
+#
+class ftep::portal(
+
+) {
+
+}
