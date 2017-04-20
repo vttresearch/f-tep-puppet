df0105827e9e90c5ff0bfcd08f4ae2c70f930674
Peter van Zetten <peter.van.zetten@cgi.com>
2017-04-20 11:39:58 +0100

    Sync with cgi-eoss/ftep development branch
    Change-Id: I34e2200b488ecc234f4acdb9e7628ed45e6672eb

--- manifests/monitor/graylog_collector.pp
+++ manifests/monitor/graylog_collector.pp
@@ -8,15 +8,17 @@ class ftep::monitor::graylog_collector (
 
   require ::ftep::globals
 
+  $config_file = '/etc/graylog/collector/collector.conf'
+
   $real_graylog_api_url = pick($graylog_api_url, "${ftep::globals::base_url}${ftep::globals::graylog_api_path}")
   $real_graylog_server = pick($graylog_server, $ftep::globals::monitor_hostname)
   $real_graylog_gelf_tcp_port = pick($graylog_gelf_tcp_port, $ftep::globals::graylog_gelf_tcp_port)
 
-  ensure_packages(['graylog-connector'], {
+  ensure_packages(['graylog-collector'], {
     ensure => 'present',
   })
 
-  file { '/etc/graylog/collector/collector.conf':
+  file { $config_file:
     ensure  => present,
     mode    => '640',
     content => epp('ftep/graylog-collector/collector.conf.epp', {
@@ -26,13 +28,16 @@ class ftep::monitor::graylog_collector (
       'graylog_server'          => $real_graylog_server,
       'graylog_gelf_tcp_port'   => $real_graylog_gelf_tcp_port,
     }),
-    require => Package['graylog-connector'],
+    require => Package['graylog-collector'],
     notify  => Service['graylog-collector'],
   }
 
   service { 'graylog-collector':
     ensure => 'running',
-    enable => true
+    enable => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require => [Package['graylog-collector'], File[$config_file]],
   }
 
 }
