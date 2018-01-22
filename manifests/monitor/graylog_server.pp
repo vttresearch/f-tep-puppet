ddf457dce0d3d8d05b1eaf8fcb8e618b0bbfb418
Peter van Zetten <peter.vanzetten@gmail.com>
2018-01-22 20:26:24 +0000

    Update Elasticsearch version

--- manifests/monitor/graylog_server.pp
+++ manifests/monitor/graylog_server.pp
@@ -1,14 +1,14 @@
 class ftep::monitor::graylog_server (
-  $db_secret            = undef,
-  $db_sha256            = undef,
-  $db_bind_ip           = '127.0.0.1',
+  $db_secret    = undef,
+  $db_sha256    = undef,
+  $db_bind_ip   = '127.0.0.1',
 
-  $listen_host          = '0.0.0.0',
-  $listen_port          = undef,
-  $context_path         = undef,
+  $listen_host  = '0.0.0.0',
+  $listen_port  = undef,
+  $context_path = undef,
 
   $graylog_repo_version = '2.4',
-  $graylog_version      = '2.4.1',
+  $graylog_version      = '2.4.3',
 ) {
 
   require ::epel
@@ -25,7 +25,7 @@ class ftep::monitor::graylog_server (
   } ->
   class { 'mongodb::server':
     bind_ip => ['127.0.0.1'],
-  }
+    }
 
   class { 'elasticsearch':
     version      => '5.6.6',
@@ -33,25 +33,25 @@ class ftep::monitor::graylog_server (
     manage_repo  => true,
   } ->
   elasticsearch::instance { 'graylog':
-    config => {
-      'cluster.name' => 'graylog',
-      'network.host' => $db_bind_ip,
+      config => {
+        'cluster.name' => 'graylog',
+        'network.host' => $db_bind_ip,
+      }
     }
-  }
 
   class { 'graylog::repository':
     version => $graylog_repo_version
   } ->
   class { 'graylog::server':
     package_version => $graylog_version,
-    config          => {
-      password_secret          => $real_db_secret, # Fill in your password secret
-      root_password_sha2       => $real_db_sha256, # Fill in your root password hash
-      web_listen_uri           => "http://${listen_host}:${real_listen_port}${real_context_path}/",
-      rest_listen_uri          => "http://${listen_host}:${real_listen_port}${real_context_path}/api/",
-      usage_statistics_enabled => false,
+      config => {
+        password_secret          => $real_db_secret, # Fill in your password secret
+        root_password_sha2       => $real_db_sha256, # Fill in your root password hash
+        web_listen_uri           => "http://${listen_host}:${real_listen_port}${real_context_path}/",
+        rest_listen_uri          => "http://${listen_host}:${real_listen_port}${real_context_path}/api/",
+        usage_statistics_enabled => false,
     },
     require         => [Yumrepo['graylog']],
-  }
+    }
 
 }
