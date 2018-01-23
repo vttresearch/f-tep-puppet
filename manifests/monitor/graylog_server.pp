e9c0dc3e1204fd485a7e74e9aa2a7f4b4ab0a4ee
Peter van Zetten <peter.van.zetten@cgi.com>
2018-01-23 09:42:51 +0000

    Update graylog to 2.4
    Change-Id: I76a6b8f87d37931aeb73f9743f4f948b5ac653e7

--- manifests/monitor/graylog_server.pp
+++ manifests/monitor/graylog_server.pp
@@ -20,38 +20,38 @@ class ftep::monitor::graylog_server (
   $real_listen_port = pick($listen_port, $ftep::globals::graylog_port)
   $real_context_path = pick($context_path, $ftep::globals::graylog_context_path)
 
-  class { 'mongodb::globals':
+  class { ::mongodb::globals:
     manage_package_repo => true,
   } ->
-  class { 'mongodb::server':
-    bind_ip => ['127.0.0.1'],
-    }
+  class { ::mongodb::server:
+    bind_ip => [$db_bind_ip],
+  }
 
-  class { 'elasticsearch':
+  class { ::elasticsearch:
     version      => '5.6.6',
     repo_version => '5.x',
     manage_repo  => true,
   } ->
-  elasticsearch::instance { 'graylog':
+  ::elasticsearch::instance { 'graylog':
       config => {
         'cluster.name' => 'graylog',
         'network.host' => $db_bind_ip,
       }
     }
 
-  class { 'graylog::repository':
+  class { ::graylog::repository:
     version => $graylog_repo_version
   } ->
-  class { 'graylog::server':
+  class { ::graylog::server:
     package_version => $graylog_version,
-      config => {
-        password_secret          => $real_db_secret, # Fill in your password secret
-        root_password_sha2       => $real_db_sha256, # Fill in your root password hash
-        web_listen_uri           => "http://${listen_host}:${real_listen_port}${real_context_path}/",
-        rest_listen_uri          => "http://${listen_host}:${real_listen_port}${real_context_path}/api/",
-        usage_statistics_enabled => false,
+    config          => {
+      password_secret          => $real_db_secret, # Fill in your password secret
+      root_password_sha2       => $real_db_sha256, # Fill in your root password hash
+      web_listen_uri           => "http://${listen_host}:${real_listen_port}${real_context_path}/",
+      rest_listen_uri          => "http://${listen_host}:${real_listen_port}${real_context_path}/api/",
+      usage_statistics_enabled => false,
     },
     require         => [Yumrepo['graylog']],
-    }
+  }
 
 }
